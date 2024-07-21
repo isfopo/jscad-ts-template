ed8ec92fa8c1251ad3782f915479f282df54a5fe
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-21 12:06:15 -0400

    adds return type

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 // An import statement allows your code to use jscad methods:
+import { Geom3 } from "@jscad/modeling/src/geometries/types";
 import { cube } from "@jscad/modeling/src/primitives";
 
 // A function declaration that returns geometry
-const main = () => {
+const main = (): Geom3 => {
   return cube();
 };
 
