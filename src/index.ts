1e5f90e801dc353ff5cc359721caaa2968830dd4
Isaac Poole <55164207+isfopo@users.noreply.github.com>
2024-07-21 12:08:49 -0400

    uses export

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,6 @@
-// An import statement allows your code to use jscad methods:
-import { Geom3 } from "@jscad/modeling/src/geometries/types";
 import { cube } from "@jscad/modeling/src/primitives";
 
 // A function declaration that returns geometry
-const main = (): Geom3 => {
+export const main = () => {
   return cube();
 };
-
-// A declaration of what elements in the module (this file) are externally available.
-module.exports = { main };
